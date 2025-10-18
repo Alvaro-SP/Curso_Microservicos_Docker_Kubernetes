9f8df08daa9b4c2ca53e9b516b47e357162395f9
Alvaro Emmanuel Socop Perez <3034161730108@ingenieria.usac.edu.gt>
2025-10-17 21:09:15 -0600

    inicio ejemplo de comunicacion con docker

---
+++ pedidos/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios'); // Client HTTP para realizar peticiones a otros servicios
+const app = express();
+const PORT = 5001;
+
+const USUARIOS_HOST = 'usuarios';
+const USUARIOS_PORT = 5000;
+
+app.get('/pedidos', async (req, res) => {
+    const url = `http://${USUARIOS_HOST}:${USUARIOS_PORT}/obtenerUsuarios`;
+    const response = await axios.get(url);
+    console.log(response);
+    const usuarios = response.data;
+    
+    //generar pedidos
+    const pedidos = usuarios.slice(0, 5).map((u, idx) => ({
+        pedido_id: idx + 1,
+        usuario: u.nombre,
+    }));
+
+    res.json({
+        mensaje: 'Se han obtenido los pedidos del sistema correctamente.',
+        data: pedidos,
+        origenUsuarios: url,
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Microservicio de Pedidos en Node.js ejecutandose en http://localhost:${PORT}/`);
+});
